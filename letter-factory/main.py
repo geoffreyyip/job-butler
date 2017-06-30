90deaab2226ed3fb2e7cfeb6b59953773e4500f2
geoffreyyip <geoffreyyip2@gmail.com>
2017-06-30 16:11:29 -0400

    Finish letter-factory script

--- letter-factory/main.py
+++ letter-factory/main.py
@@ -1,14 +1,46 @@
-import os
+from string import Template
+
 import csv
+import datetime
 
-# csv should be located in the same directory
-example_file = open('scrap.csv')
+# loads a template string into memory
+def load_template(template):
+    with open(template) as f:
+        return f.read()
 
-reader = csv.reader(example_file)
+# dumps form letters and job description to file
+def write_job_app(filename, paragraphs):
+    filepath = 'applications/' + filename
+    with open(filepath, 'w') as f:
+        for paragraph in paragraphs:
+            f.write(paragraph)
+            f.write('\n_____________________\n\n')
+
+# todo: default argument to today's date
+def make_filename(name, title, location, date=datetime.datetime.now()):
+    date_str = date.strftime('%Y.%m.%d')
+    return "{} {} {} in {}.txt".format(date_str, name, title, location)
+
+if __name__ == "__main__":
+    cover_template = load_template('cover_template.txt')
+    email_template = load_template('email_template.txt')
+
+    with open('companies.csv') as f:
+        companies = csv.DictReader(f)
+        for company in companies:
+            cover_letter = Template(cover_template).substitute(company)
+            email_letter = Template(email_template).substitute(company)
+
+            job_description = company['JOB_DESCRIPTION']
+            filename = make_filename(
+                company['COMPANY_NAME'],
+                company['JOB_TITLE'],
+                company['LOCATION'],
+            )
+
+            write_job_app(
+                filename,
+                [cover_letter, email_letter, job_description],
+            )
 
 
-
-for row in reader:
-    for field in row:
-        print(field)
-    print('__________________________\n')
